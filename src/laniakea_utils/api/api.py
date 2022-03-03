577a6759de1de56b3c7a311a93114c3a9c52e353
mtangaro <ma.tangaro@gmail.com>
2022-03-03 08:52:41 +0000

    add blueprint, set info as blueprint

--- src/laniakea_utils/api/api.py
+++ src/laniakea_utils/api/api.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask import request
+from info import info_bp
 from flaat import Flaat
 from flaat import tokentools
 import json
-
 
 import sys
 from laniakea_utils.common.read_config import ReadConfigurationFile
@@ -13,6 +13,7 @@ flaat = Flaat()
 flaat.set_web_framework('flask')
 
 app = Flask(__name__)
+app.register_blueprint(info_bp)
 
 configuration = ReadConfigurationFile()
 flaat.set_trusted_OP_list(configuration.get_trusted_OP_list())
@@ -22,32 +23,3 @@ flaat.set_trusted_OP_list(configuration.get_trusted_OP_list())
 #@flaat.login_required()
 def hello_world():
     return "<p>Laniakea Utils API</p>"
-
-@app.route('/info')
-def info():
-    access_token = tokentools.get_access_token_from_request(request)
-    info = flaat.get_info_thats_in_at(access_token)
-    # FIXME: Also display info from userinfo endpoint
-    x = json.dumps(info, sort_keys=True, indent=4, separators=(',', ': '))
-    return(str(x))
-    return("yeah")
-
-
-@app.route('/valid_user/<id>', methods=['GET'])
-@flaat.login_required()
-def valid_user_id(id):
-    access_token = tokentools.get_access_token_from_request(request)
-    info = flaat.get_info_thats_in_at(access_token)
-    # FIXME: Also display info from userinfo endpoint
-    retval=""
-    if id == info['body']['sub']:
-      retval += F'This worked: there was a valid login, and an id: {id}\n'
-    else:
-      retval += F'This failed'
-
-    return(retval)
-
-@app.route('/valid_user')
-@flaat.login_required()
-def valid_user():
-    return('This worked: there was a valid login\n')
